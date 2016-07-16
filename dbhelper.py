6a05542db9bca6f84d5f6ce16fb82f8ce3b2d796
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2016-07-16 15:37:45 -0700

    fix sql ingection issue

--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,4 @@
 import pymysql
-import dbconfig
 import os
 
 # SET your api keys as env vars
@@ -14,7 +13,7 @@ class DBHelper:
               db=database)
 
   def get_all_inputs(self):
-  connection = self.connect()
+    connection = self.connect()
     try:
       query = "SELECT description FROM crimes;"
       with connection.cursor() as cursor:
@@ -26,18 +25,18 @@ class DBHelper:
     connection = self.connect()
     try:
       # The following introduces a deliberate security flaw. See section on SQL injection below
-      query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+      query = "INSERT INTO crimes (description) VALUES (%s);"
       with connection.cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute(query,data)
         connection.commit()
     finally:
       connection.close()
-        def clear_all(self):
+  def clear_all(self):
     connection = self.connect()
     try:
       query = "DELETE FROM crimes;"
       with connection.cursor() as cursor:
         cursor.execute(query) 
-       connection.commit()
+        connection.commit()
     finally:
       connection.close()
