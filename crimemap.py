1713dbf21ca664b56d6feeef69765e49eaa2c974
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2016-07-17 22:50:31 -0700

    final crimemap

--- crimemap.py
+++ crimemap.py
@@ -3,7 +3,11 @@ from flask import render_template
 from flask import request
 import json
 import os
+import datetime
+import dateparser
+import string
 
+categories = ['mugging', 'break-in']
 TEST = os.environ.get('TEST_CRIMEAPP')
 if TEST == True:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -14,36 +18,38 @@ app = Flask(__name__)
 DB = DBHelper()
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = json.dumps(DB.get_all_crimes())
-    return render_template("home.html", crimes=crimes)
-
-@app.route("/add", methods=["POST"])
-def add():
-  try:
-    data = request.form.get("userinput")
-    DB.add_input(data)
-  except Exception as e:
-    print e
-  return home()
-
-@app.route("/clear")
-def clear():
-  try:
-    DB.clear_all()
-  except Exception as e:
-    print e
-  return home()
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
   category = request.form.get("category")
-  date = request.form.get("date") 
-  latitude = float(request.form.get("latitude"))
-  longitude = float(request.form.get("longitude"))
+  if category not in categories:
+      return home()
+  date = format_date(request.form.get("date") )
+  if not date:
+      return home("Invalid date. Please use yyyy-mm-dd format")
+  try:
+      latitude = float(request.form.get("latitude"))
+      longitude = float(request.form.get("longitude"))
+  except ValueError:
+      return home()
   description = request.form.get("description") 
+  description = sanitize_string(request.form.get("description"))
   DB.add_crime(category, date, latitude, longitude, description)
   return home()
 
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None  
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)  
+
 if __name__ == '__main__':
-  app.run(port=5000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
--- mockdbhelper.py
+++ mockdbhelper.py
@@ -1,15 +1,8 @@
 class MockDBHelper:
   def connect(self, database="crimemap"):
     pass
-  def get_all_inputs(self):
-    return []
-  def add_input(self, data):
-    pass
-  def clear_all(self):
-    pass
   def add_crime(self, category, date, latitude, longitude, description):  
     pass
-
   def get_all_crimes(self):
     return [{ 'latitude': -33.301304,
               'longitude': 26.523355,
